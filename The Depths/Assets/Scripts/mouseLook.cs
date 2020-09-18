e60c1b0eea1f2ed7b8e2eb1e07aceea9f1b3819c
agent-bear <mathijs.vankemenade@live.nl>
2020-09-18 23:09:02 +0200

    added playercontroller

---
+++ The Depths/Assets/Scripts/mouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 5f;
+    
+    float xRotation = 0f;
+    float yRotation = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation * mouseSensitivity, yRotation * mouseSensitivity, 0);
+    }
+}
---
+++ The Depths/Assets/Scripts/moveRotate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRotate : MonoBehaviour
+{
+    public Transform camTransform;
+    public Transform meshTransform;
+
+    public CharacterController controller;
+
+    public float turnSpeed = 1f;
+
+    public float moveSpeed = 12f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Lerps the rotation of the mesh to the camera
+        Quaternion camRotation = camTransform.transform.localRotation;
+        Quaternion meshRotation = Quaternion.Lerp(meshTransform.transform.localRotation, camRotation, Time.deltaTime * turnSpeed);
+        meshTransform.transform.localRotation = meshRotation;
+
+        //gets the input from the keyboard
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 meshDir =  camTransform.transform.right * x + camTransform.transform.forward * y;
+
+        controller.Move(meshDir * moveSpeed * Time.deltaTime);
+        
+    
+    
+    }
+}
